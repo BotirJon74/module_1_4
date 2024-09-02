2c484457db6ef4a3e74f8330a93f56ecab38c8d7
BotirJon74 <31174@mail.ru>
2024-09-02 05:16:36 +0300

    Add files via upload

---
+++ 'module_1_4.py'.py
@@ -0,0 +1,7 @@
+my_string='Hello, world!'
+print(len(my_string))
+print('Hello, world!'.upper())
+print('Hello, world!'.lower())
+print('Hello, world!' .replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
